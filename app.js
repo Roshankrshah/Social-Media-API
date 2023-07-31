3299466cad6f2dec91f52dda6b47292fea5f7678
Roshan <shahroshan0720@gmail.com>
2023-07-31 18:10:17 +0530

    database connected

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./db/connect');
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Social Media App API Loading");
 });
 
 const PORT = process.env.PORT || 2001;
-app.listen(PORT,()=>{
-    console.log(`Server is listening at http://localhost:${PORT}`);
-});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server is listening at http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+start();
