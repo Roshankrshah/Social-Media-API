b9b2e0127907fd8011e2d8943433b9b646e9ac83
Roshan <shahroshan0720@gmail.com>
2023-07-31 22:30:24 +0530

    unfollow user setup

--- controllers/user.js
+++ controllers/user.js
@@ -1,53 +1,92 @@
 const User = require('../models/user');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const bcrypt = require('bcrypt');
 
-const update = async (req,res)=>{
-    console.log(req.body.userId, req.params.id );
-    if(req.body.userId === req.params.id || req.body.isAdmin){
-        if(req.body.password){
-            try{
+const update = async (req, res) => {
+    console.log(req.body.userId, req.params.id);
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
                 const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(req.body.password,salt);
-            }catch(err){
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
                 return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
             }
         }
 
-        try{
-            const user = await User.findByIdAndUpdate(req.params.id,{
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
             res.status(StatusCodes.OK).json("Account has been updated");
-        }catch(error){
+        } catch (error) {
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
         }
-    }else{
+    } else {
         res.status(StatusCodes.FORBIDDEN).json('You can only update your account!');
     }
 }
 
-const deleteUser = async (req,res)=>{
+const deleteUser = async (req, res) => {
     if (req.body.userId === req.params.id || req.body.isAdmin) {
-        try{
+        try {
             await User.findByIdAndDelete(req.params.id);
             res.status(StatusCodes.OK).json('Account has been deleted');
-        }catch(err){
+        } catch (err) {
             res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
         }
-    }else{
+    } else {
         res.status(StatusCodes.FORBIDDEN).json('You can delete only your account!');
     }
 };
 
-const getUser = async (req,res)=>{
-    try{
+const getUser = async (req, res) => {
+    try {
         const user = await User.findById(req.params.id);
-        const {password,updatedAt, ...other} = user._doc;
+        const { password, updatedAt, ...other } = user._doc;
         res.status(StatusCodes.OK).json(other);
-    }catch(err){
+    } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
 }
 
-module.exports = {update, deleteUser, getUser};
+const followUser = async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
+                res.status(StatusCodes.OK).json("user has been followed");
+            } else {
+                res.status(StatusCodes.FORBIDDEN).json('you already follow this user');
+            }
+        } catch (err) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+        }
+    } else {
+        res.status(StatusCodes.FORBIDDEN).json('you cannot follow yourself');
+    }
+}
+
+const unfollowUser = async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json("user has been unfollowed");
+            } else {
+                res.status(403).json("you dont follow this user");
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("you cant unfollow yourself");
+    }
+}
+module.exports = { update, deleteUser, getUser, followUser, unfollowUser };
