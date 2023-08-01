149dce58465aa0006e86b9e673ee496ec1a5784d
Roshan <shahroshan0720@gmail.com>
2023-08-01 12:05:30 +0530

    setup all endpoints

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/connect');
+const morgan = require('morgan');
 const app = express();
+
 
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.json());
+app.use(morgan('common'));
+
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const postRoute = require('./routes/post');
+
 app.get('/', (req, res) => {
     res.send("Social Media App API Loading");
 });
+
+app.use('/api/v1/auth',authRoute);
+app.use('/api/v1/user',userRoute);
+app.use('/api/v1/posts',postRoute);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
